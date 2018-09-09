9fe52ef62aedd037e5649ce0bdafedd4a7f415f6
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2018-09-09 16:15:13 -0400

    Created default and second constructors for the class Circle

--- Week2_Assignment2/Circle.h
+++ Week2_Assignment2/Circle.h
@@ -10,7 +10,18 @@ private:
 	long int centerY;
 	double radius;
 public:
-
+	Circle() : BasicShape()
+	{
+		centerX = 0;
+		CenterY = 0;
+		radius = 0.0;
+	}
+	Circle(double a, long int cX, long int cY, double r) : BasicShape(double a)
+	{
+		centerX = cX;
+		centerY = cY;
+		radiius = r;			
+	}
 };
 
 #endif
